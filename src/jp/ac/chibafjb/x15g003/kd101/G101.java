5ebb7d22e5f8b5d2771623267f04757e9495f7f0
unknown <x15g003@PC504N016.chiba-fjb.ac.jp>
2015-11-02 14:25:32 +0900

    1

---
+++ src/jp/ac/chibafjb/x15g003/kd101/G101.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g003.kd101;
+
+public class G101 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.print("船橋情報ビジネス専門学校\n047-425-1051\nHello☆こんにちは");
+	}
+
+}
