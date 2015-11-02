7ea59024fbbe41864830e71c2478a131d330ff89
unknown <x15g003@PC504N016.chiba-fjb.ac.jp>
2015-11-02 15:01:59 +0900

    111

---
+++ src/jp/ac/chibafjb/x15g003/kd111/G111.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g003.kd111;
+
+public class G111 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		for(x=1;x<=10;x=x+1)
+		{
+			System.out.println("船橋太郎");
+		}
+
+	}
+
+}
