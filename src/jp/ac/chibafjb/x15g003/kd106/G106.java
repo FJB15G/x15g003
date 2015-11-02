e6631fdec51d718a43c45c5f8886ffc9daa7b926
unknown <x15g003@PC504N016.chiba-fjb.ac.jp>
2015-11-02 14:56:33 +0900

    106

---
+++ src/jp/ac/chibafjb/x15g003/kd106/G106.java
@@ -0,0 +1,21 @@
+package jp.ac.chibafjb.x15g003.kd106;
+
+public class G106 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println(" a@@@a a@@@a");
+		System.out.println("@@@@@@e@@@@@@");  
+		System.out.println("@@@@@@@@@@@@@"); 
+		System.out.println("@@@@@@@@@@@@@");  
+		System.out.println(" @@@@@@@@@@@");   
+		System.out.println("  ~@@@@@@@~ ");
+		System.out.println("    ~@@@~");
+		System.out.println("     ~@~ハート！");       
+
+		
+
+	}
+
+}
