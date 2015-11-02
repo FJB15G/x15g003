79f0ff651cfd7062c6cdc4f0f8d109ce85aa051e
unknown <x15g003@PC504N016.chiba-fjb.ac.jp>
2015-11-02 14:51:55 +0900

    105

---
+++ src/jp/ac/chibafjb/x15g003/kd105/G105.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g003.kd105;
+
+public class G105 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("氏名：　船橋　太郎（ふなばし　たろう)");
+		System.out.println("○○：　○○○○○○○○○○○○○○○○○○○○○○○○○○○");
+		System.out.println("○○：　○○○○○○○○○○○○○○○○○○○○○○○○○○○");
+	}
+
+}
