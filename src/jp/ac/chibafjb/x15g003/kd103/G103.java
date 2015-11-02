dfa2a3f18a85128eef3a997099360bad201bdc4d
unknown <x15g003@PC504N016.chiba-fjb.ac.jp>
2015-11-02 14:46:40 +0900

    103

---
+++ src/jp/ac/chibafjb/x15g003/kd103/G103.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g003.kd103;
+
+public class G103 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("1 船橋太郎 2 船橋太郎 3 船橋太郎 4 船橋太郎 5 船橋太郎 6 船橋太郎 7 船橋太郎 8 船橋太郎 9 船橋太郎 10 船橋太郎");
+
+	}
+
+}
