ac3ad9b49261f821ca5114f40b9ef1d4795aa16d
unknown <x15g003@PC504N016.chiba-fjb.ac.jp>
2015-11-02 15:06:07 +0900

    112

---
+++ src/jp/ac/chibafjb/x15g003/kd112/G112.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g003.kd112;
+
+public class G112 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		for(x=1;x<=1000;x=x++)
+		{
+			System.out.println("船橋情報ビジネス専門学校");
+		}
+
+	}
+
+}
